818b511d9011600a709abdcb8324ad436075c266
quassbottle <quassbottleyt@gmail.com>
2023-10-05 23:05:57 +0300

    feat: join, create course

---
+++ GroznAPi/Contracts/Course/CourseCreatedResponseDto.cs
@@ -0,0 +1,8 @@
+namespace Contracts.Course;
+
+public class CourseCreatedResponseDto
+{
+    public int CourseId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
