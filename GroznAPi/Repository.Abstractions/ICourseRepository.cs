1ad1a434c6ee67ad4a89e0699c6709008b52760e
quassbottle <quassbottleyt@gmail.com>
2023-10-05 22:33:12 +0300

    feat: course repository

--- GroznAPi/Repository.Abstractions/ICourseRepository.cs
+++ GroznAPi/Repository.Abstractions/ICourseRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Repository.Abstractions;
+﻿using Domain.Entities;
+
+namespace Repository.Abstractions;
 
 public interface ICourseRepository
 {
-    
+    Task<List<Course>> GetAllAsync();
+    Task<Course> GetByIdAsync(int id);
+    Task<Course> UpdateAsync(Course t);
+    Task CreateAsync(Course t);
+    Task DeleteAsync(Course t);
 }
