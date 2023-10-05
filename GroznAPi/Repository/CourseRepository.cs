561e89ad7838424f80526bf406abade9912490bf
quassbottle <quassbottleyt@gmail.com>
2023-10-05 22:33:12 +0300

    feat: course repository

--- GroznAPi/Repository/CourseRepository.cs
+++ GroznAPi/Repository/CourseRepository.cs
@@ -1,8 +1,58 @@
-﻿using Repository.Abstractions;
+﻿using Domain.Entities;
+using Helpers;
+using Microsoft.EntityFrameworkCore;
+using Presistence;
+using Repository.Abstractions;
 
 namespace Repository;
 
 public class CourseRepository : ICourseRepository
 {
-    
+    private readonly Context _db;
+
+    public CourseRepository(Context db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<Course>> GetAllAsync()
+    {
+        return await _db.Courses.ToListAsync();
+    }
+
+    public async Task<Course> GetByIdAsync(int id)
+    {
+        var result = await _db.Courses.FirstOrDefaultAsync(x => x.Id == id);
+        return result ?? throw new Exception("User not found");
+    }
+
+    public async Task<Course> UpdateAsync(Course t)
+    {
+        var dbCourse = await _db.Courses.FirstOrDefaultAsync(x => x.Id == t.Id);
+        if (dbCourse == null)
+        {
+            throw new Exception("Course not found");
+        }
+
+        dbCourse.Description = t.Description;
+        dbCourse.Title = t.Description;
+        dbCourse.Students = t.Students;
+        dbCourse.Teacher = t.Teacher;
+        dbCourse.Themes = t.Themes;
+            
+        await _db.SaveChangesAsync();
+        return dbCourse;
+    }
+
+    public async Task CreateAsync(Course t)
+    {
+        await _db.Courses.AddAsync(t);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Course t)
+    {
+        _db.Courses.Remove(t);
+        await _db.SaveChangesAsync();
+    }
 }
