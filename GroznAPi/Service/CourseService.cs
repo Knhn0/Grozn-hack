40aa7ee4de0739860d918b9242903449198c3d73
quassbottle <quassbottleyt@gmail.com>
2023-10-05 22:39:30 +0300

    feat: course service wip

--- GroznAPi/Service/CourseService.cs
+++ GroznAPi/Service/CourseService.cs
@@ -1,8 +1,26 @@
+using Contracts.Course;
+using Domain.Entities;
+using Repository.Abstractions;
 using Service.Abstactions;
 
 namespace Service;
 
 public class CourseService : ICourseService
 {
-    
+    private readonly ICourseRepository _courseRepository;
+
+    public CourseService(ICourseRepository courseRepository)
+    {
+        _courseRepository = courseRepository;
+    }
+
+    public async Task CreateCourseAsync(CreateCourseRequestDto request)
+    {
+        var course = new Course
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Teacher = 
+        }
+    }
 }
