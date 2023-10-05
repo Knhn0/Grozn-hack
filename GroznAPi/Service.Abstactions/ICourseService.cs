fce3bad9f608c5fd73cfef962236fed29336623f
quassbottle <quassbottleyt@gmail.com>
2023-10-06 02:26:03 +0300

    feat: course controller

--- GroznAPi/Service.Abstactions/ICourseService.cs
+++ GroznAPi/Service.Abstactions/ICourseService.cs
@@ -1,10 +1,16 @@
 ﻿using Contracts.Course;
+using Domain.Entities;
 
 namespace Service.Abstactions;
 
 public interface ICourseService
 {
-    Task<CourseCreatedResponseDto> CreateCourseAsync(CreateCourseRequestDto request);
+    Task<CourseCreatedResponseDto> CreateCourseAsync(CreateCourseRequestDto request, int userId);
     Task<CourseJoinedResponseDto> JoinCourseAsync(JoinCourseRequestDto request, int userId);
     Task<GetThemesResponseDto> GetThemesAsync(GetThemesRequestDto request);
+    Task<List<Course>> GetJoinedCourses(int userId);
+    Task<List<Course>> GetAllCoursesAsync();
+    Task<Course> GetByIdAsync(int courseId);
+    Task<CourseRemovedResponseDto> RemoveCourseForcedAsync(RemoveCourseRequestDto request);
+    Task<CourseRemovedResponseDto> RemoveCourseAsync(RemoveCourseRequestDto request, int userId);
 }
