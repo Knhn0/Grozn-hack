0d4085e99e6a7a98ec6e240b7745c45218411961
= <111117870+Knhn0@users.noreply.github.com>
2023-10-06 15:15:11 +0300

    feat: added teachers

---
+++ GroznAPi/Contracts/Teacher/GetTeacherDto.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Contracts.Teacher;
+
+public class GetTeacherDto
+{
+    public int Id { get; set; }
+
+    public UserInfo UserInfo { get; set; }
+
+    public ICollection<Domain.Entities.Course> Courses { get; set; }
+
+}
