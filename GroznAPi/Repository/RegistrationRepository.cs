3ef2f1b9d6981f2ac840e84b5109f1f0b5ec0923
Eg0rS <Ehrgit@yandex.ru>
2023-10-06 04:43:57 +0300

    fix jwt

--- GroznAPi/Repository/RegistrationRepository.cs
+++ GroznAPi/Repository/RegistrationRepository.cs
@@ -16,6 +16,24 @@ public class RegistrationRepository : IRegistrationRepository
     public async Task<Account> RegisterAsync(Account account)
     {
         var acc = await _dbContext.Accounts.AddAsync(account);
+        switch (account.UserInfo.Role.Title)
+        {
+            case "Student":
+            {
+                var student = new Student {User = acc.Entity.UserInfo};
+            
+                await _dbContext.Students.AddAsync(student);
+                break;
+            }
+            case "Teacher":
+            {
+                var teacher = new Teacher {UserInfo = acc.Entity.UserInfo};
+            
+                await _dbContext.Teachers.AddAsync(teacher);
+                break;
+            }
+        }
+
         await _dbContext.SaveChangesAsync();
         return acc.Entity;
     }
